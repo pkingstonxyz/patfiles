a475afffb048730cd34b488f0da609409f0d1456
Patrick Kingston <patrick@pkingston.xyz>
2026-05-30 15:43:00 -0400

    emacs - guixify config

---
+++ emacs/.config/emacs/early-init.el
@@ -0,0 +1,4 @@
+;; Override user emacs
+(setq user-emacs-directory "~/.config/emacs-stow/")
+;; Move custom file out of the way too
+(setq cutom-file (expand-file-name "custom.el" user-emacs-directory))
