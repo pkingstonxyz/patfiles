ed335a20eace1f66e42f3bcf98a128752e304963
Patrick Kingston <patrick@pkingston.xyz>
2026-05-31 01:05:34 -0400

    emacs,sway - configure emacs some
    - sway - use emacs server and launch it with sway
    - emacs - configure some goodies

--- emacs-stow/.config/emacs/init.el
+++ emacs-stow/.config/emacs/init.el
@@ -1,10 +1,60 @@
 (use-package tokyo-night
-  :vc (:url "https://github.com/bbatsov/tokyo-night-emacs" :rev "92037072b6e9a485d736bf8a7673193694410")
-  :config (load-theme 'tokyo-night t))
+  :vc (:url "https://github.com/bbatsov/tokyo-night-emacs" :rev "92037072b6e9a48d5d736bf8a76731936ea94410")
+  :config (load-theme 'tokyo-night-moon t))
+
+(use-package ligature
+  :vc (:url "https://github.com/mickeynp/ligature.el" :rev "6ac1634612dbd42f7eb81ecaf022bd239aabb954")
+  :config   ;; Enable the "www" ligature in every possible major mode
+  (ligature-set-ligatures 't '("www"))
+  ;; Enable traditional ligature support in eww-mode, if the
+  ;; `variable-pitch' face supports it
+  (ligature-set-ligatures 'eww-mode '("ff" "fi" "ffi"))
+  ;; Enable all Cascadia Code ligatures in programming modes
+  (ligature-set-ligatures 'prog-mode '("|||>" "<|||" "<==>" "<!--" "####" "~~>" "***" "||=" "||>"
+                                       ":::" "::=" "=:=" "===" "==>" "=!=" "=>>" "=<<" "=/=" "!=="
+                                       "!!." ">=>" ">>=" ">>>" ">>-" ">->" "->>" "-->" "---" "-<<"
+                                       "<~~" "<~>" "<*>" "<||" "<|>" "<$>" "<==" "<=>" "<=<" "<->"
+                                       "<--" "<-<" "<<=" "<<-" "<<<" "<+>" "</>" "###" "#_(" "..<"
+                                       "..." "+++" "/==" "///" "_|_" "www" "&&" "^=" "~~" "~@" "~="
+                                       "~>" "~-" "**" "*>" "*/" "||" "|}" "|]" "|=" "|>" "|-" "{|"
+                                       "[|" "]#" "::" ":=" ":>" ":<" "$>" "==" "=>" "!=" "!!" ">:"
+                                       ">=" ">>" ">-" "-~" "-|" "->" "--" "-<" "<~" "<*" "<|" "<:"
+                                       "<$" "<=" "<>" "<-" "<<" "<+" "</" "#{" "#[" "#:" "#=" "#!"
+                                       "##" "#(" "#?" "#_" "%%" ".=" ".-" ".." ".?" "+>" "++" "?:"
+                                       "?=" "?." "??" ";;" "/*" "/=" "/>" "//" "__" "~~" "(*" "*)"
+                                       "\\\\" "://"))
+  ;; Enables ligature checks globally in all buffers. You can also do it
+  ;; per mode with `ligature-mode'.
+  (global-ligature-mode t))
+
+(add-to-list 'default-frame-alist
+       '(font . "Fira Code-13"))
+
+(require 'package)
+(add-to-list 'package-archives
+             '("melpa-stable" . "https://stable.melpa.org/packages/") t)
 
 (custom-set-variables
-  '(package-selected-packages '(magit tokyo-night transient-cycles))
-  '(package-vc-selected-packages '((tokyo-night :url "https://github.com/bbatsov/tokyo-night-emacs")))
-  '(scroll-bar-mode nil))
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(menu-bar-mode nil)
+ '(package-selected-packages '(magit transient-cycles))
+ '(package-vc-selected-packages
+   '((ligature :url "https://github.com/mickeynp/ligature.el")
+     (tokyo-night :url "https://github.com/bbatsov/tokyo-night-emacs")))
+ '(scroll-bar-mode nil)
+ '(tool-bar-mode nil))
 
-(custom-set-faces)
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ )
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ 
